c0d58051a17a79ccd35075ae771e16f91db1e710
kittymacdown <kittymacdown@gmail.com>
2012-07-18 14:29:38 +0000

    demo new template

--- Source/wamp/www/e-learning-website/components/com_subject/subject.php
+++ Source/wamp/www/e-learning-website/components/com_subject/subject.php
@@ -1,6 +1,6 @@
 <?php
 defined( '_JEXEC' ) or die( 'Restricted access' );
 echo '<div class="componentheading">Thu vien ly thuyet</div>';
-echo "NamKT sexy";
+echo "NamKT sexyNamKT sexyNamKT sexyNamKT sexyNamKT sexyNamKT sexyNamKT sexyNamKT sexyNamKT sexyNamKT sexyNamKT sexyNamKT sexy";
 echo "abcdef";
 ?>
